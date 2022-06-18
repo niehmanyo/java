07abdd163e82ab5af334f3a30937cb6d17416c08
niehmanyo <1695028510@qq.com>
2022-06-18 20:25:21 +0800

    java package information

--- java_class/java_class_demo/src/java_class_demo/Main.java
+++ java_class/java_class_demo/src/java_class_demo/Main.java
@@ -1,10 +1,13 @@
 package java_class_demo;
 
 import java.util.LinkedList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        Random r  = new Random();
         ST<String,Integer> string_table = new ST<>();
         System.out.println(string_table.size);
         string_table.add("niewenyu",123);
@@ -12,6 +15,11 @@ public class Main {
         string_table.add("niewenyu1",123);
         System.out.println(string_table.size);
         string_table.PrintKey();
+        String s = new String();
+        double x = r.nextDouble();
+        double y = r.nextDouble();
+        double disx  = ;
+        
         
     }
 }
